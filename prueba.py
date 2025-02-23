042fda47095fbafd9b32926344c7a6e29202561c
JoseeMelk <moralescastellanos1234@gmail.com>
2025-02-22 22:13:44 -0600

    Agregar Services y Models para las lineas

--- prueba.py
+++ prueba.py
@@ -1,13 +1,12 @@
-from calculate_slope import CalculateSlope
-from calculate_line_direction import CalculateLineDirection
-from dda_line_class import LineaDDA
+from services.line_service import LineService
+from models.line_model import LineModel
+
 #3
 A = [10, 40]
 B = [20, 25]
-slope = CalculateSlope(A, B)
-direction = CalculateLineDirection(A,B)
-_slope = slope.slope()
-_direction = direction.line_direction()
-line = LineaDDA(A, B, _slope, _direction)
-print(f"Pendiente de A{A} a B{B}: {slope.slope()} y direccion: {_direction}")
-print(f"con la linea {line.calculate_line()}") 
+
+line_model = LineModel(A, B)
+
+line_data = LineService(line_model).calculate_line_properties()
+
+print(line_data)
